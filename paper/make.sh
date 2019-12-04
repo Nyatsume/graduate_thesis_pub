c182e9d0231832ec19b43c93b59b09135b268214
yas-nyan <yas-nyan@sfc.wide.ad.jp>
2019-12-04 22:51:48 +0900

    add git ignore

--- paper/make.sh
+++ paper/make.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest dvipdfmx thesis.dvi
