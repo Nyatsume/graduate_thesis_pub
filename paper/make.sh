204d77c755de589222e15aa3177bc74d79ab5e71
yas-nyan <yas-nyan@sfc.wide.ad.jp>
2019-12-12 04:36:53 +0900

    add bibtex

--- paper/make.sh
+++ paper/make.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
+docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest pbibtex thesis
+docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest dvipdfmx thesis.dvi
